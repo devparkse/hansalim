d74d860278a2dc8b5dade480c80d48d7d897cc32
Park Sieun <pmj8856@naver.com>
2023-07-02 18:01:59 +0900

    모달창 추가

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@
 // 모든 요소가 갖추어지면 실행하도록 한다.
 // window.onload = function () {} 은 한번만 작성한다.
 window.onload = function () {
+  AOS.init();
+
+  // 모달창
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal");
+  modal.addEventListener("click", function () {
+    // modal.style.display = "none";
+    // fadeOut(modal);
+    anime({
+      targets: ".modal",
+      // delay: 200,
+      duration: 500,
+      opacity: 0,
+      easing: "easeInOutQuad",
+      complete: function () {
+        modal.style.display = "none";
+        body.classList.add("active");
+      },
+    });
+  });
+
   // 콤마 기능
   function priceToString(price) {
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
